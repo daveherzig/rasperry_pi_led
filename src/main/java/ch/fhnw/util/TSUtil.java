77eea3b0e1548d8a8d5c1268f8f97551b1aac65a
dorgut <dorgut@gmail.com>
2019-03-08 16:29:03 +0100

    Dokumentation ergänzt.

--- src/main/java/ch/fhnw/util/TSUtil.java
+++ src/main/java/ch/fhnw/util/TSUtil.java
@@ -5,16 +5,32 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
+ * Utility class for the Raspberry Pi LED Project
  * @author David Herzig
  */
 public class TSUtil {
     
+	/**
+	 * Wandelt timestamp (long in ms) in ein lesbares Format (string in dd-MM-yy HH:mm:ss). 
+	 * @param timestamp
+	 * @return
+	 */
     public static String convertTimeStamp(long timestamp) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         String result  = dateFormat.format(new Date(timestamp));
         return result;
     }
     
+    /**
+     * TimeStamp ausgeben.
+     * @param year
+     * @param month
+     * @param day
+     * @param hour
+     * @param minute
+     * @param second
+     * @return
+     */
     public static long getTimeStamp(String year, String month, String day, String hour, String minute, String second) {
         String timeStr = year + "-" + month + "-" + day + ":" + hour + "-" + minute + "-" + second;
         System.out.println(timeStr);
