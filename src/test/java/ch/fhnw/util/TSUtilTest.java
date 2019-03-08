e348c5d2eef2485441a5f1e1653f373c5e9e2662
thomasnebl <thomasnebl@DESKTOP-R2VLVN7.edu.ds.fhnw.ch>
2019-03-08 19:46:07 +0100

    Ergänzt mit Tests

---
+++ src/test/java/ch/fhnw/util/TSUtilTest.java
@@ -0,0 +1,19 @@
+package ch.fhnw.util;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class TSUtilTest {
+
+	@Test
+	public void testConvertTimeStamp() {
+       long timestamp = 1551733443855l;
+       String expectedValue = "04-03-2019 22:04:03";
+       String value = TSUtil.convertTimeStamp(timestamp);
+       assertEquals(expectedValue, value);
+	}
+	
+}
