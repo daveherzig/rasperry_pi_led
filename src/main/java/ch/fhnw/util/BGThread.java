ddba4ac62f674f55dac005a2f6505668ead8f7d0
Born <Born@LAPTOP-GKBG383K.edu.ds.fhnw.ch>
2019-03-09 11:20:51 +0100

    final

--- src/main/java/ch/fhnw/util/BGThread.java
+++ src/main/java/ch/fhnw/util/BGThread.java
@@ -1,7 +1,10 @@
 package ch.fhnw.util;
 
 import java.util.List;
-
+	/** @author Born
+	 * Tolles Programm
+	 * 
+	 */
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
